f58d0d84814795e036d88697cc6c71b295fd8c8e
MinTruong <86372849+MinTruong@users.noreply.github.com>
2022-08-24 18:20:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,14 +56,11 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
+    LOG.info("JSON payload: %s json_payload")
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
-    # scale the input
+    LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
-    # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
