ea5d6a02c79942a0b5cfa0bf54e29be1e423013e
MinhTruong <minhcm2@gmail.com>
2022-08-29 21:59:27 +0700

    modify name app

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-az webapp up --name flaskapp205386 --resource-group Azuredevops --runtime "PYTHON:3.7" -l southcentralus
+az webapp up --name flaskapp-minh --resource-group Azuredevops --runtime "PYTHON:3.7" -l southcentralus
